f685c566c311d99f614ddea056fe9fe6a2b9d7b5
Dmitrii Gusev <amidg4x4dima@gmail.com>
2024-07-29 22:40:32 -0700

    working day 1

--- 2015/1/main.cpp
+++ 2015/1/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include "input.h"
 
 int curr_floor = 0;
+int basement_index = 0; // first index that makes Santa enter the basement. Indeces start with 1
 
 int main(int argc, char* argv[]) {
-    for (; *puzzle_input != '\0'; puzzle_input++) {
+    std::cout << strlen(puzzle_input) << std::endl;
+    for (int i = 1; *puzzle_input != '\0'; puzzle_input++, i++) {
+        // calculate current floor index
         if (*puzzle_input == '(')
             curr_floor++;
         else if (*puzzle_input == ')')
             curr_floor--;
+
+        // calculate index at which santa enters basement for the first time
+        if (curr_floor < 0 && basement_index == 0)
+            basement_index = i;
     }
 
-    std::cout << "Resulting Floor: " << (int)curr_floor << std::endl;
+    std::cout << "Resulting Floor: " << curr_floor << std::endl;
+    std::cout << "First index to enter basement: " << basement_index << std::endl;
 
     return 0;
 }
